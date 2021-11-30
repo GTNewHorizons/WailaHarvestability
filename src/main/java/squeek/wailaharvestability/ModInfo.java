9f00b359596247c67c75560a310adda678987eea
Johann Bernhardt <johann.bernhardt@tum.de>
2021-11-30 23:26:09 +0100

    move sources and resources

--- src/main/java/squeek/wailaharvestability/ModInfo.java
+++ src/main/java/squeek/wailaharvestability/ModInfo.java
@@ -3,5 +3,6 @@ package squeek.wailaharvestability;
 public final class ModInfo
 {
 	public static final String MODID = "WailaHarvestability";
-	public static final String VERSION = "${version}";
+	public static final String MODNAME = "Waila Harvestability";
+	public static final String VERSION = "GRADLETOKEN_VERSION";
 }
